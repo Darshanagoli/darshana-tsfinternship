ada2e18c7d9bf723d4f68cec4e65efffe9fbe59e
Darshanagoli <90027447+Darshanagoli@users.noreply.github.com>
2021-10-16 08:26:26 -0700

    Create viewcustomer.js

---
+++ viewcustomer.js
@@ -0,0 +1,75 @@
+function customer1()
+{
+    document.getElementById("customer1").style.display="block";
+    document.getElementById("mainbody").style.display="none";
+    document.getElementById("darshanagoliBankBalance1").innerHTML=document.getElementById("darshanagoliBankBalance").innerHTML;
+}
+function customer2()
+{
+    document.getElementById("customer2").style.display="block";
+    document.getElementById("mainbody").style.display="none";
+    document.getElementById("shreyasigoliBankBalance1").innerHTML=document.getElementById("shreyasigoliBankBalance").innerHTML;
+}
+function customer3()
+{
+    document.getElementById("customer3").style.display="block";
+    document.getElementById("mainbody").style.display="none";
+    document.getElementById("ramadevigoliBankBalance1").innerHTML=document.getElementById("ramadevigoliBankBalance").innerHTML;
+}
+function customer4()
+{
+    document.getElementById("customer4").style.display="block";
+    document.getElementById("mainbody").style.display="none";
+    document.getElementById("govardhangoliBankBalance1").innerHTML=document.getElementById("govardhangoliBankBalance").innerHTML;
+}
+function customer5()
+{
+    document.getElementById("customer5").style.display="block";
+    document.getElementById("mainbody").style.display="none";
+    document.getElementById("shubhamshriramBankBalance1").innerHTML=document.getElementById("shubhamshriramBankBalance").innerHTML;
+}
+function customer6()
+{
+    document.getElementById("customer6").style.display="block";
+    document.getElementById("mainbody").style.display="none";
+    document.getElementById("ruchikavadnalBankBalance1").innerHTML=document.getElementById("ruchikavadnalBankBalance").innerHTML;
+}
+function customer7()
+{
+    document.getElementById("customer7").style.display="block";
+    document.getElementById("mainbody").style.display="none";
+    document.getElementById("mayurmacharlaBankBalance1").innerHTML=document.getElementById("mayurmacharlaBankBalance").innerHTML;
+}
+function customer8()
+{
+    document.getElementById("customer8").style.display="block";
+    document.getElementById("mainbody").style.display="none";
+    document.getElementById("monikakondiBankBalance1").innerHTML=document.getElementById("monikakondiBankBalance").innerHTML;
+}
+function customer9()
+{
+    document.getElementById("customer9").style.display="block";
+    document.getElementById("mainbody").style.display="none";
+    document.getElementById("shriharshagangulBankBalance1").innerHTML=document.getElementById("shriharshagangulBankBalance").innerHTML;
+}
+function customer10()
+{
+    document.getElementById("customer10").style.display="block";
+    document.getElementById("mainbody").style.display="none";
+    document.getElementById("ramgangulBankBalance1").innerHTML=document.getElementById("ramgangulBankBalance").innerHTML;
+}
+function back()
+{
+    document.getElementById("mainbody").style.display="block";
+    document.getElementById("customer1").style.display="none";
+    document.getElementById("customer2").style.display="none";
+    document.getElementById("customer3").style.display="none";
+    document.getElementById("customer4").style.display="none";
+    document.getElementById("customer5").style.display="none";
+    document.getElementById("customer6").style.display="none";
+    document.getElementById("customer7").style.display="none";
+    document.getElementById("customer8").style.display="none";
+    document.getElementById("customer9").style.display="none";
+    document.getElementById("customer10").style.display="none";
+
+}
